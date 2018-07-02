9f08e89d5155624af05673f531e96b57177df08a
Fulvio Corno <fulvio.corno@polito.it>
2018-07-02 16:58:48 +0200

    Tema d'esame e progetto base

---
+++ src/it/polito/tdp/extflightdelays/ExtFlightDelaysController.java
@@ -0,0 +1,85 @@
+/**
+ * Sample Skeleton for 'ExtFlightDelays.fxml' Controller Class
+ */
+
+package it.polito.tdp.extflightdelays;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.extflightdelays.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ExtFlightDelaysController {
+
+	private Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML // fx:id="compagnieMinimo"
+    private TextField compagnieMinimo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnAnalizza"
+    private Button btnAnalizza; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmbBoxAeroportoPartenza"
+    private ComboBox<?> cmbBoxAeroportoPartenza; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnAeroportiConnessi"
+    private Button btnAeroportiConnessi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmbBoxAeroportoDestinazione"
+    private ComboBox<?> cmbBoxAeroportoDestinazione; // Value injected by FXMLLoader
+
+    @FXML // fx:id="numeroTratteTxtInput"
+    private TextField numeroTratteTxtInput; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaItinerario"
+    private Button btnCercaItinerario; // Value injected by FXMLLoader
+
+    @FXML
+    void doAnalizzaAeroporti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCalcolaAeroportiConnessi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaItinerario(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+        assert compagnieMinimo != null : "fx:id=\"compagnieMinimo\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+        assert btnAnalizza != null : "fx:id=\"btnAnalizza\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+        assert cmbBoxAeroportoPartenza != null : "fx:id=\"cmbBoxAeroportoPartenza\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+        assert btnAeroportiConnessi != null : "fx:id=\"btnAeroportiConnessi\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+        assert cmbBoxAeroportoDestinazione != null : "fx:id=\"cmbBoxAeroportoDestinazione\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+        assert numeroTratteTxtInput != null : "fx:id=\"numeroTratteTxtInput\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+        assert btnCercaItinerario != null : "fx:id=\"btnCercaItinerario\" was not injected: check your FXML file 'ExtFlightDelays.fxml'.";
+
+    }
+    
+    
+    public void setModel(Model model) {
+  		this.model = model;
+  		
+  	}
+}
